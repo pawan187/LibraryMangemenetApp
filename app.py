9997b41b1c32722ad9771d4a5455e86d63871e87
pawan suthar <35306329+pawan187@users.noreply.github.com>
2021-04-08 16:32:36 +0530

    Add files via upload
    sql CRUD for Books added.

--- app.py
+++ app.py
@@ -3,46 +3,24 @@ import pyodbc
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=PHZPDSQLDB2K12;'
                       'Database=testfoript;'
-                      "UID=testiptdbo;"
+                      "Uid=testiptdbo;"
                       "PWD=testiptdbo123;")
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM [testfoript].[Cure].[CureUser]')
+#cursor.execute('SELECT * FROM [testfoript].[Cure].[CureUser]')
 
-for row in cursor:
-    print(row)
+#for row in cursor:
+#    print(row)
 
 app = Flask(__name__)
-BookData = [ {
-     "Id" : 1,
-     "name" : "Xyz",
-     "author" :"abc",
-     "qty" : 3,
-     "location" : "selve 5",
-     "rent" : 10
-        }
-    ,
-    {
-     "Id" : 2,
-     "name" : "LMN",
-     "author" :"yui",
-     "qty" : 2,
-     "location" : "selve 4",
-     "rent" : 15
-        }
-    ]
-members = [{
-    "Id" : 1,
-    "name":"pawan",
-    "contact":"7977763748",
-    "balance" : 0
-    }]
+BookData = []
+members = []
 
 transaction = [
     {
-        "Id" : 1,
-        "BookId":1,
-        "UserId":1,
+        "id" : 1,
+        "Bookid":1,
+        "Userid":1,
         "date":"xyz"
 }]
 @app.route('/')
@@ -52,54 +30,74 @@ def index():
 @app.route('/Books', methods=['GET','POST'])
 def AddBook():
    if request.method == 'GET':
-       cursor.execute("SELECT * FROM [testfoript].[dbo].[BooksLibrary];") 
+       BootData = []
+       cursor.execute("SELECT * FROM [testfoript].[dbo].[LibararyBooks];") 
        row = cursor.fetchone() 
        while row: 
-           print(row[0])
+           BookData.append(
+           {
+            "id" : row[0],
+            "name" : row[1],
+            "author" :row[2],
+            "charge": row[3],
+            "location" : row[4],
+            "qty" : row[5]
+            })
            row = cursor.fetchone()
        data = jsonify(BookData)
        print(data)
        return data
    elif request.method == 'POST':
        data = request.json
-       BookData.append(data)
+       count = cursor.execute("INSERT INTO [testfoript].[dbo].[LibararyBooks] (name, author,charge, qty, location) VALUES (?,?,?,?,?)",data['name'] , data['author'],data['charge'] ,data['qty'],data['location'])
+       cursor.commit()
        print(BookData)
        return data
 @app.route('/Books/<id>', methods=['PUT','DELETE'])
 def EditBook(id):
    if request.method == 'PUT':
-       for i in range(0,len(BookData)):
-           if BookData[i]["Id"] == int(id):
-               print(request.args.get('name'))
-               BookData[i]["name"] = request.args.get('name')
-       data = jsonify(BookData)
+       data = request.json
+       query = cursor.execute("UPDATE [dbo].[LibararyBooks]    SET [name] = ?      ,[author] = ?,[charge] = ?,[location] = ?,[qty] = ? WHERE id = ? ",data['name'] , data['author'],data['charge'] ,data['location'],data['qty'],id)
+       cursor.commit()
+       data = jsonify("successfull")
        print(data)
        return data
    elif request.method == 'DELETE':
-       for i in range(0,len(BookData)):
-           print(BookData[i]["Id"]==int(id))
-           if BookData[i]["Id"] == int(id):
-               BookData.pop(i)
-       data = jsonify(BookData)
+       count = cursor.execute("DELETE FROM [dbo].[LibararyBooks] WHERE id like '"+ id +"'")
+       count.commit()
+       data = jsonify("done")
        print(BookData)
        return data
  
-@app.route('/memebers', methods=['GET','POST'])
+@app.route('/members', methods=['GET','POST'])
 def AddMemebers():
    if request.method == 'GET':
+       cursor.execute("SELECT * FROM [testfoript].[dbo].[MembersLibrary];") 
+       row = cursor.fetchone() 
+       while row: 
+           members.append(
+           {
+            "id" : row[0]
+      ,"Mname" :row[1]
+      ,"contact" :row[2]
+      ,"balance" : row[3]
+      ,"address" : row[4]
+            })
+           row = cursor.fetchone()
        data = jsonify(members)
        print(data)
        return data
    elif request.method == 'POST':
        data = request.json
-       members.append(data)
+       count = cursor.execute("INSERT INTO [testfoript].[dbo].[MembersLibrary] (id, Mname, contact, balance, address) VALUES (?,?,?,?,?)",data['id'],data['name'], data['contact'],str(data['balance']),data['address'])
+       cursor.commit()
        print(members)
        return data
-@app.route('/memebers/<id>', methods=['PUT','DELETE'])
+@app.route('/members/<id>', methods=['PUT','DELETE'])
 def EditMemebers(id):
    if request.method == 'PUT':
        for i in range(0,len(members)):
-           if members[i]["Id"] == int(id):
+           if members[i]["id"] == int(id):
                print(request.args.get('name'))
                members[i]["name"] = request.args.get('name')
        data = jsonify(members)
@@ -107,8 +105,8 @@ def EditMemebers(id):
        return data
    elif request.method == 'DELETE':
        for i in range(0,len(members)):
-           print(members[i]["Id"]==int(id))
-           if members[i]["Id"] == int(id):
+           print(members[i]["id"]==int(id))
+           if members[i]["id"] == int(id):
                members.pop(i)
        data = jsonify(members)
        print(members)
@@ -129,7 +127,7 @@ def AddTransaction():
 def EditTransaction(id):
    if request.method == 'PUT':
        for i in range(0,len(transaction)):
-           if transaction[i]["Id"] == int(id):
+           if transaction[i]["id"] == int(id):
                print(request.args.get('date'))
                transaction[i]["date"] = request.args.get('date')
        data = jsonify(transaction)
@@ -137,8 +135,8 @@ def EditTransaction(id):
        return data
    elif request.method == 'DELETE':
        for i in range(0,len(transaction)):
-           print(transaction[i]["Id"]==int(id))
-           if transaction[i]["Id"] == int(id):
+           print(transaction[i]["id"]==int(id))
+           if transaction[i]["id"] == int(id):
                transaction.pop(i)
        data = jsonify(transaction)
        print(transaction)

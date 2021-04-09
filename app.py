bd9e712e9f591484d45dc7c708dce9d81b381e30
pawan suthar <35306329+pawan187@users.noreply.github.com>
2021-04-09 17:43:24 +0530

    Add files via upload
    Sql CRUD from members and transaction Get and delete added.

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ cursor = conn.cursor()
 #    print(row)
 
 app = Flask(__name__)
-BookData = []
-members = []
-
 transaction = [
     {
         "id" : 1,
@@ -30,7 +27,7 @@ def index():
 @app.route('/Books', methods=['GET','POST'])
 def AddBook():
    if request.method == 'GET':
-       BootData = []
+       BookData = []
        cursor.execute("SELECT * FROM [testfoript].[dbo].[LibararyBooks];") 
        row = cursor.fetchone() 
        while row: 
@@ -51,7 +48,7 @@ def AddBook():
        data = request.json
        count = cursor.execute("INSERT INTO [testfoript].[dbo].[LibararyBooks] (name, author,charge, qty, location) VALUES (?,?,?,?,?)",data['name'] , data['author'],data['charge'] ,data['qty'],data['location'])
        cursor.commit()
-       print(BookData)
+       print(data)
        return data
 @app.route('/Books/<id>', methods=['PUT','DELETE'])
 def EditBook(id):
@@ -72,7 +69,8 @@ def EditBook(id):
 @app.route('/members', methods=['GET','POST'])
 def AddMemebers():
    if request.method == 'GET':
-       cursor.execute("SELECT * FROM [testfoript].[dbo].[MembersLibrary];") 
+       members = []
+       cursor.execute("SELECT * FROM [testfoript].[dbo].[LibraryMembers];") 
        row = cursor.fetchone() 
        while row: 
            members.append(
@@ -89,59 +87,66 @@ def AddMemebers():
        return data
    elif request.method == 'POST':
        data = request.json
-       count = cursor.execute("INSERT INTO [testfoript].[dbo].[MembersLibrary] (id, Mname, contact, balance, address) VALUES (?,?,?,?,?)",data['id'],data['name'], data['contact'],str(data['balance']),data['address'])
+       count = cursor.execute("INSERT INTO [testfoript].[dbo].[LibraryMembers] ( Mname, contact, balance, address) VALUES (?,?,?,?)",data['Mname'], data['contact'],str(data['balance']),data['address'])
        cursor.commit()
-       print(members)
+       print(count)
        return data
 @app.route('/members/<id>', methods=['PUT','DELETE'])
 def EditMemebers(id):
    if request.method == 'PUT':
-       for i in range(0,len(members)):
-           if members[i]["id"] == int(id):
-               print(request.args.get('name'))
-               members[i]["name"] = request.args.get('name')
-       data = jsonify(members)
+       data = request.json
+       count = cursor.execute("update [dbo].[LibraryMembers]  set [Mname] = ?, contact = ?, balance = ?, address = ? where id = ?",data['name'], data['contact'],str(data['balance']),data['address'],id)
+       cursor.commit()
+       data = jsonify("edited")
        print(data)
        return data
    elif request.method == 'DELETE':
-       for i in range(0,len(members)):
-           print(members[i]["id"]==int(id))
-           if members[i]["id"] == int(id):
-               members.pop(i)
-       data = jsonify(members)
-       print(members)
+       count = cursor.execute("DELETE FROM [dbo].[LibraryMembers] WHERE id like '"+ id +"'")
+       count.commit()
+       data = jsonify("deleted!")
+       print(count)
        return data
  
 @app.route('/transaction', methods=['GET','POST'])
 def AddTransaction():
    if request.method == 'GET':
-       data = jsonify(transaction)
+       Transactions =[]
+       cursor.execute("SELECT *  FROM [testfoript].[dbo].[LibraryTransactions]")
+       row = cursor.fetchone()
+       while row:
+           Transactions.append({
+               "id" : row[0],
+               "Bid" : row[1],
+               "Mid": row[2],
+               "date":row[3],
+               "status" : row[4]
+               })
+           row = cursor.fetchone()
+       data = jsonify(Transactions)
        print(data)
        return data
    elif request.method == 'POST':
        data = request.json
+       row = cursor.execute("SET NOCOUNT ON; declare @rc int; exec @rc = [dbo].[LibraryInsertTransaction] Bid= ?, Mid= ?,TDate=  ?, Tstatus = ? select @rc as rc", data["Bid"], data["Mid"] , str(data["TDate"]), data["Tstatus"])
+       row.commit()
        transaction.append(data)
-       print(transaction)
+       print(row)
        return data
 @app.route('/transaction/<id>', methods=['PUT','DELETE'])
 def EditTransaction(id):
    if request.method == 'PUT':
-       for i in range(0,len(transaction)):
-           if transaction[i]["id"] == int(id):
-               print(request.args.get('date'))
-               transaction[i]["date"] = request.args.get('date')
-       data = jsonify(transaction)
+       data = request.json
+       row = cursor.execute("update [testfoript].[dbo].[LibraryTransactions] set Bid = ? , Mid = ? ,TDate = ? , Tstatus = ? where id = ?" , data["Bid"], data["Mid"] , str(data["TDate"]), data["Tstatus"], id)
+       row.commit()
+       data = jsonify(row)
        print(data)
        return data
    elif request.method == 'DELETE':
-       for i in range(0,len(transaction)):
-           print(transaction[i]["id"]==int(id))
-           if transaction[i]["id"] == int(id):
-               transaction.pop(i)
-       data = jsonify(transaction)
-       print(transaction)
+       row = row.execute("dalete from [testfoript].[dbo].[LibraryTransactions] where id - ? " , id)
+       row.commit()
+       data = jsonify(row)
+       print(data)
        return data
  
-
 if __name__ == '__main__':
     app.run(debug=True)
